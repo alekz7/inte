9ef696e762a3082fa2e555731c8a0325e20e1ad3
Alejandro Aragon <ing.aragon.ipn@gmail.com>
2020-08-19 10:47:50 -0500

    carpeta con el frontend

---
+++ src/frontend/build/precache-manifest.46381c03c750563fa685cc88a6c30d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f808bb3a5363c3ae4fb98710736e94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ce5c008da71408d7721",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4ce5c008da71408d7721",
+    "url": "/static/js/2.20a3ca0d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.20a3ca0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1eae25fc3633e102a06",
+    "url": "/static/js/main.22d78c7e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
